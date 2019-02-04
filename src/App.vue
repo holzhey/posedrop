399e51cd1fba2145d14057f487a009632932f2ce
Alexandre Holzhey <alexandre.holzhey@pdmfc.com>
2019-02-04 14:10:26 +0000

    Better behaviour and protptype

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <Box class="box" id="b1" :onValueChange="{ x : change }"/>
-    <div class="zone" id="z1">TESTE TESTE TESTE TESTE</div>
-    <p v-if="over">HOVERED</p>
+    <Box class="box" :onValueChange="{ x : changeX, y: changeY }"/>
+    <div class="zone">TESTE 1</div>
+    <div class="zone">TESTE 2</div>
+    <div class="zone">TESTE 3</div>
   </div>
 </template>
 
@@ -18,21 +19,50 @@ export default {
   },
   data() {
     return {
-      over: false,
+      over: null,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
-    change(x) {
-      let e = document.getElementById('z1');
-      let b = document.getElementById('b1');
-      e = e.getBoundingClientRect();
-      b = b.getBoundingClientRect();
-      const eX = e.left;
-      const eX2 = e.right;
-      const bX = b.left + x;
-      const bX2 = bX + b.width;
-      console.log(eX, bX, eX2, bX2);
-      this.over = (bX >= eX && bX2 <= eX2);
+    changeY(y) {
+      this.y = y + 50;
+      this.checkBellow();
+    },
+    changeX(x) {
+      this.x = x + 50;
+      this.checkBellow();
+    },
+    getDistanceTo(htmlElement) {
+      const elementCenterX = htmlElement.offsetLeft + (htmlElement.clientWidth / 2);
+      const elementCenterY = htmlElement.offsetTop + (htmlElement.clientHeight / 2);
+      const radius = Math.sqrt(
+        ((this.x - elementCenterX) ** 2) + ((this.y - elementCenterY) ** 2)
+      );
+      return {
+        radius: radius,
+        element: htmlElement,
+      };
+    },
+    checkBellow() {
+      const zones = document.getElementsByClassName('zone');
+      if (zones) {
+        const zonesRadius = Array.prototype.map.call(zones, this.getDistanceTo);
+        const zone = zonesRadius.filter(item => item.radius < 200)
+          .sort((a, b) => {
+            return b.radius - a.radius;
+          })
+          .pop();
+        if (zone && zone.element !== this.over) {
+          if (zone && zone.element) {
+            zone.element.style.background = 'darkorange';
+          }
+          if (this.over) {
+            this.over.style.background = 'aliceblue';
+          }
+          this.over = zone.element;
+        }
+      }
     },
   },
 };
@@ -52,5 +82,22 @@ body {
   width: 100px;
   height: 100px;
   background: #54e365;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+
+.zone {
+  width: 220px;
+  height: 220px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  border: 2px solid;
+  border-color: rgb(253, 255, 73);
+  text-align: center;
+  background: aliceblue;
+  transition: all 0.3s ease-in-out;
 }
 </style>
